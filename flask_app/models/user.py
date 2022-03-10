99c41d8db33842f22c7f2d29d23bbaf748807fba
Jack Knoell <knoelljack@gmail.com>
2022-03-10 14:35:18 -0800

    updated vendor and user model with get_all methods

--- flask_app/models/user.py
+++ flask_app/models/user.py
@@ -1,4 +1,4 @@
-
+from flask_app.config.mysqlconnection import connectToMySQL
 
 class User:
     def __init__(self,data) -> None:
@@ -8,4 +8,13 @@ class User:
         self.email = data['email']
         self.created_at = data['created_at']
         self.updated_at = data['updated_at']
+
+    @classmethod
+    def get_all(cls):
+        query = "SELECT * FROM users"
+        users_from_db = connectToMySQL('users').query_db(query)
+        users = []
+        for user in users_from_db:
+            users.append(cls(user))
+        return users
 
--- flask_app/models/vendor.py
+++ flask_app/models/vendor.py
@@ -1,3 +1,5 @@
+from flask_app.config.mysqlconnection import connectToMySQL
+
 class Vendor:
     def __init__(self, data) -> None:
         self.id = data['id']
@@ -6,3 +8,12 @@ class Vendor:
         self.email = data['email']
         self.created_at = data['created_at']
         self.updated_at = data['updated_at']
+
+    @classmethod
+    def get_all(cls):
+        query = "SELECT * FROM vendors"
+        vendors_from_db = connectToMySQL('vendors').query_db(query)
+        vendors = []
+        for vendor in vendors_from_db:
+            vendors.append(vendor)
+        return vendors
