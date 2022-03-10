185bbe6c37ab83dfb6523f8e9e939f7aa2c7c386
Sydnie <sydniegreenwood@gmail.com>
2022-03-10 08:16:08 -0800

    Added CRUD SECTIONS to vendor controller

--- flask_app/controllers/vendors.py
+++ flask_app/controllers/vendors.py
@@ -1,3 +1,8 @@
 from flask_app import app
 from flask import render_template, redirect, request, session, flash
 from flask_app.models import vendor
+
+# CREATE ROUTE------------------------------------
+# READ ROUTE--------------------------------------
+# UPDATE ROUTE------------------------------------
+# DELETE ROUTE------------------------------------
