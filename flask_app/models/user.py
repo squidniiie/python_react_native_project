a1ae46ee143618f322db3b265765d33182d21096
Jack Knoell <knoelljack@gmail.com>
2022-03-09 16:30:14 -0800

    first commit

---
+++ flask_app/models/user.py
@@ -0,0 +1,11 @@
+
+
+class User:
+    def __init__(self,data) -> None:
+        self.id = data['id']
+        self.first_name = data['first_name']
+        self.last_name = data['last_name']
+        self.email = data['email']
+        self.created_at = data['created_at']
+        self.updated_at = data['updated_at']
+
