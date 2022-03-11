8053ab084f414420cfc9d89ce168f2ffbd36512d
Jack Knoell <knoelljack@gmail.com>
2022-03-10 17:22:21 -0800

    updated models with more methods and installed Bcrypt

--- flask_app/models/vendor.py
+++ flask_app/models/vendor.py
@@ -1,3 +1,4 @@
+from flask_app import DB
 from flask_app.config.mysqlconnection import connectToMySQL
 
 class Vendor:
@@ -11,9 +12,9 @@ class Vendor:
 
     #GET ALL VENDORS FROM DB
     @classmethod
-    def get_all(cls):
+    def get_all_vendors(cls):
         query = "SELECT * FROM vendors"
-        vendors_from_db = connectToMySQL('vendors').query_db(query)
+        vendors_from_db = connectToMySQL(DB).query_db(query)
         vendors = []
         for vendor in vendors_from_db:
             vendors.append(vendor)
@@ -23,5 +24,12 @@ class Vendor:
     @classmethod
     def save(cls,data):
         query = "INSERT INTO vendors (name,location,email,created_at,updated_at) VALUES (%(name)s,%(location)s,%(email)s,NOW(),NOW());"
-        vendor_id = connectToMySQL('vendors').query_db(query,data)
+        vendor_id = connectToMySQL(DB).query_db(query,data)
         return vendor_id
+
+    #GET VENDOR BY ID
+    @classmethod
+    def get_user_by_id(cls,data):
+        query = "SELECT * FROM vendors WHERE id = %(id)s;"
+        vendor_from_db = connectToMySQL(DB).query_db(query,data)
+        return cls(vendor_from_db)
